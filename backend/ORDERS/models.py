c3ae2e2e153939a2c509eb8ef9e2dc76dd2747fe
Alora-taken <63038255+Alora-taken@users.noreply.github.com>
2024-02-01 04:30:26 +0330

    make base model

--- backend/ORDERS/models.py
+++ backend/ORDERS/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from backend.models import BaseModel
 
 # Create your models here.
+class Order(BaseModel):
+    pass
---
+++ shoping_site/shoping_site/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        # Exclude soft-deleted items by default
+        return super().get_queryset().filter(deleted_at__isnull=True)
+    
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now = True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    
+    class Meta:
+       abstract=True # Set this model as Abstract
+
+    defman = models.Manager()
+    objects = SoftDeleteManager()
+
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = timezone.now()
+        self.save()
